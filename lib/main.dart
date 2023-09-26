4a02f0200db8d3350ec09fab7ee025840d9e3e43
Anurag Ashok <131293260+Anurag-Ashok@users.noreply.github.com>
2023-09-26 10:30:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google/home.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: home(),
+  ));
+}
